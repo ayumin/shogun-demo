72dfea0d0bb5e443c027040ccaab3080bde12ac3
Ayumu AIZAWA <ayumu.aizawa@gmail.com>
2016-09-01 15:05:55 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shogun::Application.config.secret_token = 'bf5f125dae060633d17ddaf685219baf16e5320656a395aa519bc9b1153f2ffdeee804ef5eca57ef3fab3f3b44709be6365b6262d4a4dad86cc231cf87aae09e'
